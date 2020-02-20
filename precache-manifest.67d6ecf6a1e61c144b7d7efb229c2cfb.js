e528899c0d91e2b5710eea3ef3a3fe1fc07fd33c
zaid <khanzaid786786.kz@gmail.com>
2020-02-20 15:53:46 +0530

    Updates

---
+++ precache-manifest.67d6ecf6a1e61c144b7d7efb229c2cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac47e73c4aa5ddbc28617890f1538e62",
+    "url": "/parking_web/index.html"
+  },
+  {
+    "revision": "42bc92f3709e0d2232e7",
+    "url": "/parking_web/static/css/main.f6d05738.chunk.css"
+  },
+  {
+    "revision": "fa971bb4ea4534f30952",
+    "url": "/parking_web/static/js/2.313c610e.chunk.js"
+  },
+  {
+    "revision": "a2d15e25411719b17b558cedadae071d",
+    "url": "/parking_web/static/js/2.313c610e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42bc92f3709e0d2232e7",
+    "url": "/parking_web/static/js/main.864a7370.chunk.js"
+  },
+  {
+    "revision": "8c5f5ca9de62d9d2529d",
+    "url": "/parking_web/static/js/runtime-main.d1b2d42a.js"
+  }
+]);
